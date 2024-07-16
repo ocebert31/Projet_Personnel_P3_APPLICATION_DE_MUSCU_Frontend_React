709f626406aa53607320a0be30df4292fb8afe77
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-07-16 13:03:01 +0200

    header feature terminé

---
+++ musculation/src/Header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+function header() {
+    return (
+        <div className='style-header'>
+            <div>FlexForce</div>
+            <button>Connexion / inscription</button>
+        </div>
+    )
+}
+
+export default header;
